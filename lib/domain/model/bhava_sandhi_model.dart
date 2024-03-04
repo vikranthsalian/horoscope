e3352adb791f53ae0e6660b1ddec1ba56b3f61ea
vikranth <vikkysalian@gmail.com>
2024-03-05 00:57:27 +0530

    Bhava Sandhi Done

---
+++ lib/domain/model/bhava_sandhi_model.dart
@@ -0,0 +1,7 @@
+class BhavaSandhiModel {
+  int? bhava;
+  String? madhya;
+  String? anthya;
+
+  BhavaSandhiModel({this.bhava, this.madhya, this.anthya});
+}
---
+++ lib/presentation/screens/horoscope/view/tabs/bhava_sandhi_screen.dart
@@ -0,0 +1,130 @@
+import 'dart:ui';
+
+import 'package:adithya_horoscope/core/constants/color_constants.dart';
+import 'package:adithya_horoscope/core/utils/calculate.dart';
+import 'package:adithya_horoscope/domain/model/bhava_sandhi_model.dart';
+import 'package:adithya_horoscope/presentation/widgets/column_view.dart';
+import 'package:adithya_horoscope/presentation/widgets/row_view.dart';
+import 'package:adithya_horoscope/presentation/widgets/style.dart';
+import 'package:adithya_horoscope/presentation/widgets/text_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BhavaSandhiScreen extends StatelessWidget {
+  final double lagnaValue, dasamaValue;
+  BhavaSandhiScreen({required this.lagnaValue, required this.dasamaValue});
+
+  List<BhavaSandhiModel> list = [];
+  List<String> headers = ["bhava", "madhya", "anthya"];
+  var padding = EdgeInsets.symmetric(horizontal: 8.w);
+  @override
+  Widget build(BuildContext context) {
+    list = HoroScopeUtils().getMetaBhavasandhiValues(lagnaValue, dasamaValue);
+
+    return SingleChildScrollView(
+      child:
+          MetaColumnView(mainAxisAlignment: MainAxisAlignment.start, children: [
+        MetaRowView(
+            children: headers.asMap().entries.map((e) {
+          return Expanded(
+            child: Container(
+              child: MetaTextView(
+                text: e.value,
+                textStyle: MetaStyle(
+                    fontSize: 12,
+                    fontColor: MetaColors.color3F3F3F,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+          );
+        }).toList()),
+        SizedBox(
+          height: 10.h,
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            scrollDirection: Axis.vertical,
+            itemCount: list.length,
+            itemBuilder: (ctx, i) {
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: MetaRowView(children: [
+                  Expanded(
+                      child: Container(
+                          child: rowText(list[i].bhava!.toString(),
+                              enabled: true))),
+                  Expanded(
+                      child:
+                          Container(child: superScriptText(list[i].madhya!))),
+                  Expanded(
+                      child:
+                          Container(child: superScriptText(list[i].anthya!))),
+                ]),
+              );
+            })
+      ]),
+    );
+  }
+
+  Widget superScriptText(String e, {bool enabled = false}) {
+    List array = e.split("SS");
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RichText(
+          text: TextSpan(
+            text: array[0],
+            style: MetaStyle(
+                    fontSize: 12,
+                    fontColor: MetaColors.color3F3F3F,
+                    fontWeight: FontWeight.w100)
+                .getStyle(),
+            children: <TextSpan>[
+              TextSpan(
+                text: " ",
+                style: MetaStyle(
+                        fontSize: 4,
+                        fontFeatures: [FontFeature.superscripts()],
+                        fontColor: MetaColors.color3F3F3F,
+                        fontWeight: FontWeight.w100)
+                    .getStyle(),
+              ),
+              TextSpan(
+                text: "ˢ",
+                style: MetaStyle(
+                        fontSize: 15,
+                        fontFeatures: [FontFeature.superscripts()],
+                        fontColor: MetaColors.color3F3F3F,
+                        fontWeight: FontWeight.w100)
+                    .getStyle(),
+              ),
+              TextSpan(
+                text: array[1],
+                style: MetaStyle(
+                        fontSize: 12,
+                        fontColor: MetaColors.color3F3F3F,
+                        fontWeight: FontWeight.w100)
+                    .getStyle(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget rowText(String e, {bool enabled = false}) {
+    return Container(
+      child: MetaTextView(
+        text: e,
+        textStyle: MetaStyle(
+            fontSize: 12,
+            fontColor:
+                enabled ? MetaColors().primaryColor : MetaColors.color3F3F3F,
+            fontWeight: FontWeight.w100),
+      ),
+    );
+  }
+}
