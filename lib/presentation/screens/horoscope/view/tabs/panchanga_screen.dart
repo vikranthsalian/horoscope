b860d7840387aa6d587902fb4bc3984372be6907
vikranth <vikkysalian@gmail.com>
2024-03-05 00:57:47 +0530

    Panchanga Done

---
+++ lib/presentation/screens/horoscope/view/tabs/panchanga_screen.dart
@@ -0,0 +1,46 @@
+import 'package:adithya_horoscope/core/constants/color_constants.dart';
+import 'package:adithya_horoscope/presentation/widgets/row_view.dart';
+import 'package:adithya_horoscope/presentation/widgets/style.dart';
+import 'package:adithya_horoscope/presentation/widgets/text_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PanchangaScreen extends StatelessWidget {
+  PanchangaScreen({required this.list});
+
+  List<(String, String)> list = [];
+  var padding = EdgeInsets.symmetric(horizontal: 8.w);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            scrollDirection: Axis.vertical,
+            itemCount: list.length,
+            itemBuilder: (ctx, i) {
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 10.h),
+                child: MetaRowView(children: [
+                  Expanded(
+                      child:
+                          Container(child: rowText(list[i].$1, enabled: true))),
+                  Expanded(child: Container(child: rowText(list[i].$2))),
+                ]),
+              );
+            }));
+  }
+
+  Widget rowText(String e, {bool enabled = false}) {
+    return Container(
+      child: MetaTextView(
+        text: e,
+        textStyle: MetaStyle(
+            fontSize: 12,
+            fontColor:
+                enabled ? MetaColors().primaryColor : MetaColors.color3F3F3F,
+            fontWeight: FontWeight.w100),
+      ),
+    );
+  }
+}
